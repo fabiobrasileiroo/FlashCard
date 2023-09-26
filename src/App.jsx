626f2d773e1e09c45fd942332eb8ee9ee81ba7c9
fabiobrasileiroo <fabio.h591@gmail.com>
2023-09-25 22:30:37 -0400

    repositorio adicionado

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import './App.css'
+import Card from './components/Card/Card'
+import CardUser from './components/Card/CardUser'
+
+function App() {
+  const item1 = {
+    nome: 'Rick Sanchez',
+    imagemUrl: 'https://rickandmortyapi.com/api/character/avatar/1.jpeg'
+  }
+
+  // TODO: Criar um segundo item e exibir um card para ele
+  const item2 = {
+    nome: 'Morty Smith',
+    imagemUrl: 'https://rickandmortyapi.com/api/character/avatar/2.jpeg'
+  }
+
+  const itens = [item1, item2]
+
+  return (
+    <>
+      {itens.map(function (item) {
+        return <Card item={item}/>
+      })}
+      <CardUser />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Card/CardUser.jsx
@@ -0,0 +1,15 @@
+import "./CardUse.css"
+
+export default function Card() {
+     return <div className="cardUser">
+        <h2>Fábio Brasileiro</h2>
+
+        <div className="tags">
+          <div className="tag">Status: sobrevivendo apenas</div>
+          <div className="tag">Espécie: Humano</div>
+          <div className="tag">Origem: Onde tudo começou</div>
+        </div>
+
+        <img src="https://media.licdn.com/dms/image/D4D03AQH-SWM-UiriJg/profile-displayphoto-shrink_800_800/0/1688764383583?e=1701302400&v=beta&t=lF_TSY2eXxFSxwjFbDeHs6E8nLS4Cw7rD7DMSmEjlqc" />
+      </div> 
+}
