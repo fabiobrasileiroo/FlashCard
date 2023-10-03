7fc139738cccef92f37a371c600d11c0ca71d7bd
Fábio Henrique Mendes Brasileiro <99563281+fabiobrasileiroo@users.noreply.github.com>
2023-10-03 01:55:52 +0000

    falta ajustar o menu

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,4 @@
-import "./Header.css";
+/* import "./Header.css";
 
 export default function Header() {
   return (
@@ -15,6 +15,48 @@ export default function Header() {
         <a href="/">Home</a>
 
         <a href="/criar-personagem">Criar personagem</a>
+      </nav>
+    </header>
+  );
+} */
+import React, { useState } from "react";
+import "./Header.css";
+
+export default function Header() {
+  const [menuVisible, setMenuVisible] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuVisible(!menuVisible);
+  };
+
+  return (
+    <header className="header">
+      <a href="/" className="logo-link">
+        <img
+          className="logo"
+          src="https://raw.githubusercontent.com/fabiobrasileiroo/Curriculo/main/assets/img/favicon.ico"
+          alt="Logo do Ocean"
+        />
+      </a>
+
+      <button className="menu-toggle" onClick={toggleMenu}>
+        Menu
+      </button>
+
+      <nav className={`menu ${menuVisible ? "menu-active" : ""}`}>
+        <ul>
+          <li className="menu-item">
+            <a href="/" className="menu-link">
+              Home
+            </a>
+          </li>
+          <li className="menu-item">
+            <a href="/criar-personagem" className="menu-link">
+              Criar personagem
+            </a>
+          </li>
+          {/* Adicione mais itens de menu conforme necessário */}
+        </ul>
       </nav>
     </header>
   );
